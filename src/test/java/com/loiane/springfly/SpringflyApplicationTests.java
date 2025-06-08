239c396907a76cf0e6f461ea3dee54211ac780e2
Loiane <loianeg@gmail.com>
2025-06-08 10:43:07 -0400

    Remove unused test method from SpringflyApplicationTests

--- src/test/java/com/loiane/springfly/SpringflyApplicationTests.java
+++ src/test/java/com/loiane/springfly/SpringflyApplicationTests.java
@@ -1,13 +1,10 @@
 package com.loiane.springfly;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringflyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
